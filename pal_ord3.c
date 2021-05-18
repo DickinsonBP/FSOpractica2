b0fbdd94c8cb9c1fecfd568951306b0a2489d584
Dickinson Bedoya Perez <dickinsonbp@gmail.com>
2021-05-18 20:29:18 +0200

    fasjfdfd

---
+++ pal_ord3.c
@@ -0,0 +1,5 @@
+#include <stdio.h> /* incloure definicions de funcions estandard */
+#include <stdlib.h>
+#include "winsuport2.h" /* incloure definicions de funcions propies */
+#include <pthread.h>
+#include <stdint.h>
